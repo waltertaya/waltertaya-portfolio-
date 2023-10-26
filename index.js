906fea84f44f9477fc55d9080d3af7adc52ac714
waltertaya <waltermittybecker5@gmail.com>
2023-10-26 15:20:14 +0300

    phone design in progress

--- index.js
+++ index.js
@@ -6,3 +6,21 @@ function showNav() {
         document.getElementById("navbar").style.display="none";
     }
 }
+let sections = document.querySelectorAll("section");
+let navbar = document.querySelectorAll("nav a");
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navbar.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector("nav a[href*=" + id + "]").classList.add('active');
+            });
+        };
+    });
+};
